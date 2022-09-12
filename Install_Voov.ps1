e3d6e0d91bd0183ec748fb799f7455b504cd8e52
Mohamed <Mohammed_ha@yahoo.com>
2022-09-12 14:15:11 +0100

    make exe copy

--- Install_Voov.ps1
+++ Install_Voov.ps1
@@ -1,5 +1,8 @@
+# Copy item [Voov deletes the exe after install FFS]
+Copy-Item -path ".\VooVMeeting_1410000197_3.9.3.510.publish.exe" -Destination "C:\temp\VooVMeeting_1410000197_3.9.3.510.publish.exe"
+
 # Install App
-Start-Process "VooVMeeting_3.3.2.510.exe" -ArgumentList "/S"
+Start-Process "C:\temp\VooVMeeting_1410000197_3.9.3.510.publish.exe" -ArgumentList "/S"
 
 start-sleep 30
 
@@ -8,7 +11,7 @@ try {
     Copy-Item "C:\Program Files (x86)\Tencent\VooVMeeting\voovmeetingapp_new.exe" "C:\Program Files (x86)\Tencent\VooVMeeting\voovmeetingapp.exe"
 }
 catch {
-    Write-Verbose "VooVMeeting_3.3.2.510.exe still installing..." -Verbose
+    Write-Verbose "VooVMeeting_3.9.3.510 still installing..." -Verbose
     start-sleep 20
 }
 
@@ -16,21 +19,21 @@ try {
     Copy-Item "C:\Program Files (x86)\Tencent\VooVMeeting\voovmeetingapp_new.exe" "C:\Program Files (x86)\Tencent\VooVMeeting\voovmeetingapp.exe"
 }
 catch {
-    Write-Verbose "VooVMeeting_3.3.2.510.exe still installing..." -Verbose
+    Write-Verbose "VooVMeeting_3.9.3.510 still installing..." -Verbose
     start-sleep 20
 }
 
 # create Shortcut
 
 function Set-ShortCut {
-[CmdletBinding()]
-param(
-[string]$SourceExe, [string]$DestinationPath
-)
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($DestinationPath)
-$Shortcut.TargetPath = $SourceExe
-$Shortcut.Save()
+    [CmdletBinding()]
+    param(
+        [string]$SourceExe, [string]$DestinationPath
+    )
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($DestinationPath)
+    $Shortcut.TargetPath = $SourceExe
+    $Shortcut.Save()
 }
 
 $PublicDesktop = $env:public + "\desktop"
