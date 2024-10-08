a5fb91f9570bb51b59a8e28dbafceb5926782130
Mohamed <Mohammed_ha@yahoo.com>
2024-10-08 15:10:26 +0100

    replace Write-Host

--- Active Directory/Backup GPOs.ps1
+++ Active Directory/Backup GPOs.ps1
@@ -3,7 +3,7 @@
 $Domainname = $Env:Userdnsdomain 
 
 # Find All Gpos In The Current Domain 
-Write-Host "[$((Get-Date).TimeofDay)] INFO: Finding All The Gpos In $Domainname" -ForegroundColor Yellow
+Write-Output "[$((Get-Date).TimeofDay)] INFO: Finding All The Gpos In $Domainname" -ForegroundColor Yellow
 Import-Module Grouppolicy 
 $Allgposindomain = Get-Gpo -All -Domain $Domainname 
 $Path = Read-Host "Where Do You Want To Backup GPO?"
@@ -13,10 +13,10 @@ $Date = (Get-Date).Tostring('dd.MM.yyyy')
 $Gpo = New-Item -Itemtype "Directory" -Path "$Path\Gpo.Backup.$Date"
 
 # Backing Up Gpo
-Write-Host "[$((Get-Date).TimeofDay)] INFO: Backing Up All The Gpos In $Domainname" 
+Write-Output "[$((Get-Date).TimeofDay)] INFO: Backing Up All The Gpos In $Domainname" 
 Set-Location -Path $path
 Foreach ($Allgpos In $Allgposindomain) {
-    Write-Host "[$((Get-Date).TimeofDay)] INFO: Backing Up ("$Allgpos.Displayname")"
+    Write-Output "[$((Get-Date).TimeofDay)] INFO: Backing Up ("$Allgpos.Displayname")"
     New-Item -ItemType "Directory" -Path "$Gpo\$($Allgpos.Displayname)"
     Backup-Gpo -Name $Allgpos.Displayname -Path "$Gpo\$($Allgpos.Displayname)" -Comment "Monthly Backup"
     Get-GPOReport -Name $Allgpos.Displayname -ReportType HTML -Path "$($GPO.Fullname)\$($Allgpos.Displayname)\$($Allgpos.Displayname).html" 
@@ -25,4 +25,4 @@ Foreach ($Allgpos In $Allgposindomain) {
 Set-Location -Path "C:\"
 Clear-Host
 $GPOsCount = Get-ChildItem -Path $Gpo | Measure-Object
-Write-host "`n[INFO] $($GPOsCount.Count) GPOs have been backed up to $Gpo" -ForegroundColor Yellow
+Write-Output "[$((Get-Date).TimeofDay)] INFO: $($GPOsCount.Count) GPOs have been backed up to $Gpo" -ForegroundColor Yellow
